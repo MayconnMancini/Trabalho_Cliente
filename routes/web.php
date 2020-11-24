bebea57f228b813dbdb70d14dd5425b71f2101e0
MayconnMancini <mayconnkayto@hotmail.com>
2020-11-24 16:50:46 -0400

    Modificado o arquivos routes/web

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\IndexController;
 
 
-Route::get('/', IndexController::class)->name('index'); 
-Route::resource('clientes', ClienteController::class);
+Route::get('/', IndexController::class)->name('index'); // configura a rota do /.
+Route::resource('clientes', ClienteController::class);  // configura a rota de clientes.
